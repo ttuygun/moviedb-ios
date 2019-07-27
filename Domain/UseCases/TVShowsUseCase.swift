ed58e99d76006c16e8dbabd7b405e46165a8c030
Tarık Taha Uygun <ttahauygun@gmail.com>
2019-07-27 16:14:33 +0300

    domain use cases added

---
+++ Domain/UseCases/TVShowsUseCase.swift
@@ -0,0 +1,13 @@
+//
+//  TVShowsUseCase.swift
+//  Domain
+//
+//  Created by Tarık Taha Uygun on 27.07.2019.
+//  Copyright © 2019 Tarık Taha Uygun. All rights reserved.
+//
+
+import Foundation
+
+public protocol TVShowsUseCase {
+
+}
---
+++ Domain/UseCases/UseCaseProvider.swift
@@ -0,0 +1,15 @@
+//
+//  UseCaseProvider.swift
+//  Domain
+//
+//  Created by Tarık Taha Uygun on 27.07.2019.
+//  Copyright © 2019 Tarık Taha Uygun. All rights reserved.
+//
+
+import Foundation
+
+public protocol UseCaseProvider {
+    func makeMoviesUseCase() -> MoviesUseCase
+    func makeTVShowsUseCase() -> TVShowsUseCase
+    func makeProfileUseCase() -> ProfileUseCase
+}
