9452f6dfb59472126ff4479b575c05dcc35144a7
Tarık Taha Uygun <ttahauygun@gmail.com>
2019-07-27 15:55:12 +0300

    Domain and NetworkPlaform added

---
+++ Domain/Domain.h
@@ -0,0 +1,19 @@
+//
+//  Domain.h
+//  Domain
+//
+//  Created by Tarık Taha Uygun on 27.07.2019.
+//  Copyright © 2019 Tarık Taha Uygun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Domain.
+FOUNDATION_EXPORT double DomainVersionNumber;
+
+//! Project version string for Domain.
+FOUNDATION_EXPORT const unsigned char DomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Domain/PublicHeader.h>
+
+
---
+++ NetworkPlatform/NetworkPlatform.h
@@ -0,0 +1,19 @@
+//
+//  NetworkPlatform.h
+//  NetworkPlatform
+//
+//  Created by Tarık Taha Uygun on 27.07.2019.
+//  Copyright © 2019 Tarık Taha Uygun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkPlatform.
+FOUNDATION_EXPORT double NetworkPlatformVersionNumber;
+
+//! Project version string for NetworkPlatform.
+FOUNDATION_EXPORT const unsigned char NetworkPlatformVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkPlatform/PublicHeader.h>
+
+
